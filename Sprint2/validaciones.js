519869889517c9c23b03be4b22e8f21c0c36b0f9
Francolu1s <franco.luis@ive..com>
2021-10-12 07:53:54 -0500

    conexion html con javascript

--- Sprint2/validaciones.js
+++ Sprint2/validaciones.js
@@ -1,37 +1,38 @@
-/*var string = 'Hola'*/
+// /*var string = 'Hola'*/
 
-function validar_nombre_usuario(string){
-    if(string.length>=6 & string.length<=30){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
+//function validar_nombre_usuario(string){
+    // if(string.length>=6 & string.length<=30){
+    //     return true;
+    // }
+    // else{
+    //     return false;
+    // }
+//}
 
 function validar_contrasena(string){
-    if(string.length>=6){
-        return true;
-    }
-    else{
-        return false;
-    }
+    // if(string.length>=6){
+    //     return true;
+    // }
+    // else{
+    //return false;
+    // }
+    alert("Esto es una alerta")
 }
 
-function confirmar_contrasena(stringA, stringB){
-    if(stringB.length>=6){
-        if(stringA=stringB){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-    else{
-        return false;
-    }
-}
+// function confirmar_contrasena(stringA, stringB){
+//     if(stringB.length>=6){
+//         if(stringA=stringB){
+//             return true;
+//         }
+//         else{
+//             return false;
+//         }
+//     }
+//     else{
+//         return false;
+//     }
+// }
 
-module.exports.validar_nombre_usuario = validar_nombre_usuario;
-module.exports.validar_contrasena=validar_contrasena;
-module.exports.confirmar_contrasena=confirmar_contrasena;
+//module.exports.validar_nombre_usuario = validar_nombre_usuario;
+//module.exports.validar_contrasena=validar_contrasena;
+//module.exports.confirmar_contrasena=confirmar_contrasena;
