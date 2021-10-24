be240283e57b602ac5405342391a1e21d1b62090
Camila <cbenitezc@unal.edu.co>
2021-10-23 23:01:46 -0500

    Se borra los campos despues de oprimir el boton, ya no considera el el boton_registro como null y entra a la funcion

--- Sprint2/validaciones.js
+++ Sprint2/validaciones.js
@@ -1,25 +1,33 @@
-const string =document.getElementById("#in_nombre_usuario");
-const stringA =document.getElementById("#in_contrasena");
-const stringB =document.getElementById("#in_confirmar_contrasena");
-const boton_registro =document.getElementById("#btn_registrar_usuario");
+const string =document.querySelector("#in_nombre_usuario");
+const stringA =document.querySelector("#in_contrasena");
+const stringB =document.querySelector("#in_confirmar_contrasena");
+const boton_registro =document.querySelector("#btn_registrar_usuario");
+const espacioMensaje=document.querySelector(".mensaje_error")
 
 const patronNombre = /^[a-zA-Z]{6,30}$/g;
 const patronContrasena = /^[A-Z0-9]{6,}$/g;
 
 let registros =[];
 
-boton_registro.addEventListener("click", agregarRegistro());
+//if(boton_registro!=null){
+boton_registro.addEventListener("click", agregarRegistro);
+//}
 
 class MiConstructor{
-    constructor(){
-        this.usuario=string.value;
-        this.contrasena=stringA.value;
-        this.confirmacion=stringB.value;
+    constructor(usuario,contrasena,confirmacion){
+        this.usuario=usuario;
+        this.contrasena=contrasena;
+        this.confirmacion=confirmacion;
     }
 }
 
 function agregarRegistro(){
-    let usuarioCreado=new MiConstructor();
+    const usuario=string.value;
+    const contrasena=stringA.value;
+    const confirmacion=stringB.value;
+
+    let usuarioCreado=new MiConstructor(usuario,contrasena,confirmacion);
+    
     if(patronNombre.test(usuarioCreado.usuario)==true){
         if(patronContrasena.test(usuarioCreado.contrasena)==true){
             if(patronContrasena.test(usuarioCreado.confirmacion)==true){
@@ -33,7 +41,12 @@ function agregarRegistro(){
         }
     }else{console.log("El usuario no es apto");
     }
+
+    string.value="";
+    stringA.value="";
+    stringB.value="";
 }
+
 /*
 function OrdenarArreglo(arreglo){}
 
